1b57388af34ad8f08aeb5e9740262e94e881f51b
TipsyBoy <gudah13@naver.com>
2022-08-26 02:19:52 +0900

    Initial commit and Test

---
+++ src/test/java/com/myboardproject/mbp/MbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myboardproject.mbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
