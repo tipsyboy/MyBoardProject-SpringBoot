db3558230494c2db5f177238998c8467c3535aca
TipsyBoy <gudah13@naver.com>
2022-09-06 15:41:32 +0900

    Member Entity 수정

--- src/main/java/com/myboardproject/mbp/domain/member/Member.java
+++ src/main/java/com/myboardproject/mbp/domain/member/Member.java
@@ -18,7 +18,13 @@ public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
 
     @OneToMany(mappedBy = "author")
     private List<Post> postList = new ArrayList<>();
