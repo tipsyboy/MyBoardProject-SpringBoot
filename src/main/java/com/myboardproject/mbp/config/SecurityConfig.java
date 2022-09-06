10212fa9eef334b29ae59081958706fa9cdeafeb
TipsyBoy <gudah13@naver.com>
2022-09-06 15:35:27 +0900

    csrf로 인한 h2-console 페이지 거부 해결

--- src/main/java/com/myboardproject/mbp/config/SecurityConfig.java
+++ src/main/java/com/myboardproject/mbp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +14,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests().antMatchers("/**").permitAll();
+                .authorizeRequests().antMatchers("/**").permitAll()
+                .and()
+                    .csrf().ignoringAntMatchers("/h2-console/**")
+                .and()
+                    .headers()
+                    .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+
 
         return http.build();
     }
