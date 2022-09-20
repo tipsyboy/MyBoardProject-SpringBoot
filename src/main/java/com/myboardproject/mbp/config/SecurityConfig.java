558bf273165a11ac5e334a2335c4962c10b64ea2
TipsyBoy <gudah13@naver.com>
2022-09-20 16:30:01 +0900

    로그아웃 구현

--- src/main/java/com/myboardproject/mbp/config/SecurityConfig.java
+++ src/main/java/com/myboardproject/mbp/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @RequiredArgsConstructor
 @Configuration
@@ -33,7 +34,12 @@ public class SecurityConfig {
                 .and()
                     .formLogin()
                     .loginPage("/member/login")
-                    .defaultSuccessUrl("/");
+                    .defaultSuccessUrl("/")
+                .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                    .logoutSuccessUrl("/")
+                    .invalidateHttpSession(true);
 
         return http.build();
     }
